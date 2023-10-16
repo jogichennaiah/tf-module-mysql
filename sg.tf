174daec37528abe7218481f3c44ee23fdb7dbe4a
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-16 21:32:02 +0530

    Auto push

---
+++ sg.tf
@@ -0,0 +1,28 @@
+# Creating security group
+
+resource "aws_security_group" "allows_mysql" {
+  name        = "Roboshop allows docdb internal traffic only"
+  description = "Allows only private traffic"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_CIDR
+
+
+  ingress {
+    description      = "mysql from private Network"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-mysql-sg"
+  }
+}
