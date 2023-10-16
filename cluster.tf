27ecd8cb450b7021c2f3cb294fd98603a3131ceb
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-16 21:59:58 +0530

    Auto push

--- cluster.tf
+++ cluster.tf
@@ -1,16 +1,18 @@
 # Creates RDS instance 
 
 resource "aws_db_instance" "mysql" {
-  allocated_storage    = 10
-  db_name              = "roboshop-${var.ENV}-mysql"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "admin1"
-  password             = "roboshop1"
-  parameter_group_name = aws_db_parameter_group.default.name
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.mysql.name
+  identifier             = "roboshop-${var-ENV}-mysql"
+  allocated_storage      = 10
+  db_name                = "roboshop-${var.ENV}-mysql"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t3.micro"
+  username               = "admin1"
+  password               = "roboshop1"
+  parameter_group_name   = aws_db_parameter_group.default.name
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.mysql.name
+  vpc_security_group_ids = [aws_security_group.allows_mysql.id]
 }
 
 resource "aws_db_subnet_group" "mysql" {
