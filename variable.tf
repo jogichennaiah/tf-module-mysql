7b10c44645ea0371ab1cce8ce65e3ed4f369610e
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-02 23:54:49 +0530

    Auto push

--- variable.tf
+++ variable.tf
@@ -1,4 +1,9 @@
 variable "ENV" {}
+
+#MySQL Variables
 variable "MYSQL_STORAGE" {}
 variable "MYSQL_ENGINE_VERSION" {}
 variable "MYSQL_INSTANCE_TYPE" {}
+
+#DocDB Variables
+variable "DOCDB_INSTANCE_TYPE" {}
