88b014bd9bc6106d8f0f6be44180105e0a7ef689
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-07 00:18:38 +0530

    Auto push

--- route53.tf
+++ route53.tf
@@ -1,8 +1,7 @@
-resource  "aws_route53_record" "rds_dns" {
-    zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS 
-    name    = "mysql-$(var.ENA).$(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_NAME)"
-    type    = "CNAME"
-    ttl     = 10
-    records  = [aws_db_instance.mysql.address] 
+resource "aws_route53_record" "rds_dns" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID 
+  name    = "mysql-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_db_instance.mysql.address]
 }
-
